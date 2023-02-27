1e2d34adfa46ec5999eb6e0521d5ba0a8a8f3f5f
Nikko Kujanpää <nikko.kujanpaa@gmail.com>
2023-02-27 21:37:22 +0200

    finish e1.6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,3 +1,57 @@
-const App = () => <h1>Hello, world!</h1>;
+import { useState } from "react";
+
+const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}> {text} </button>
+);
+
+const Statistics = ({ feedback }) => {
+    const { good, neutral, bad } = feedback;
+
+    return (
+        <>
+            <h1>statistics</h1>
+            <div>
+                good {good}
+                <br />
+                neutral {neutral}
+                <br />
+                bad {bad}
+                <br />
+            </div>
+        </>
+    );
+};
+
+const App = () => {
+    const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
+
+    const incrementFeedback = (name) => ({
+        ...feedback,
+        [name]: feedback[name] + 1,
+    });
+
+    return (
+        <>
+            <h1>give feedback</h1>
+
+            <Button
+                handleClick={() => setFeedback(incrementFeedback("good"))}
+                text={"good"}
+            />
+
+            <Button
+                handleClick={() => setFeedback(incrementFeedback("neutral"))}
+                text={"neutral"}
+            />
+
+            <Button
+                handleClick={() => setFeedback(incrementFeedback("bad"))}
+                text={"bad"}
+            />
+
+            <Statistics feedback={feedback} />
+        </>
+    );
+};
 
 export default App;
