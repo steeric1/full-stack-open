b31c3ed36158d6c0e7b3fbf1f194d8adf895951f
Nikko Kujanpää <nikko.kujanpaa@gmail.com>
2023-02-27 20:21:13 +0200

    finish e1.3

--- part1/kurssitiedot/src/App.js
+++ part1/kurssitiedot/src/App.js
@@ -6,7 +6,9 @@ const Header = (props) => (
 
 const Part = (props) => (
     <>
-        <p>{props.name} {props.exercises}</p>
+        <p>
+            {props.name} {props.exercises}
+        </p>
     </>
 );
 
@@ -16,38 +18,41 @@ const Content = (props) => (
         <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
         <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
     </>
-)
+);
 
 const Total = (props) => (
     <>
-        <p>Number of exercises {props.parts.map(p => p.exercises).reduce((accumulator, current) => accumulator + current, 0)}</p>
+        <p>
+            Number of exercises{" "}
+            {props.parts
+                .map((p) => p.exercises)
+                .reduce((accumulator, current) => accumulator + current, 0)}
+        </p>
     </>
-)
+);
 
 const App = () => {
-    const course = 'Half Stack application development'
-    const parts = [
-        {
-            name: 'Fundamentals of React',
-            exercises: 10
-        },
-        {
-            name: 'Using props to pass data',
-            exercises: 7
-        },
-        {
-            name: 'State of a component',
-            exercises: 14
-        }
-    ]
+    const course = "Half Stack application development";
+    const part1 = {
+        name: "Fundamentals of React",
+        exercises: 10,
+    };
+    const part2 = {
+        name: "Using props to pass data",
+        exercises: 7,
+    };
+    const part3 = {
+        name: "State of a component",
+        exercises: 14,
+    };
 
     return (
-    <div>
-        <Header course={course} />
-        <Content parts={parts} />
-        <Total parts={parts} />
-    </div>
-    )
-}
+        <div>
+            <Header course={course} />
+            <Content parts={[part1, part2, part3]} />
+            <Total parts={[part1, part2, part3]} />
+        </div>
+    );
+};
 
-export default App
+export default App;
