b043b26ab891d6f15bde99639fb10a5821b2ea10
Nikko Kujanpää <nikko.kujanpaa@gmail.com>
2023-02-28 21:20:15 +0200

    finish e2.7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,6 +6,11 @@ const App = () => {
 
     const addPerson = (event) => {
         event.preventDefault();
+        if (persons.find((person) => person.name === newName)) {
+            alert(`${newName} is already added to phonebook`);
+            return;
+        }
+
         const newPerson = { name: newName };
 
         setPersons([...persons, newPerson]);
