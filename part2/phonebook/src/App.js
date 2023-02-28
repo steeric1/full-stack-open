26ba9351d983be3f1285381b1856e86f23627047
Nikko Kujanpää <nikko.kujanpaa@gmail.com>
2023-02-28 21:16:05 +0200

    finish e2.6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,20 +4,37 @@ const App = () => {
     const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
     const [newName, setNewName] = useState("");
 
+    const addPerson = (event) => {
+        event.preventDefault();
+        const newPerson = { name: newName };
+
+        setPersons([...persons, newPerson]);
+        setNewName("");
+    };
+
     return (
-        <div>
+        <>
             <h2>Phonebook</h2>
-            <form>
+
+            <form onSubmit={addPerson}>
                 <div>
-                    name: <input />
+                    name:{" "}
+                    <input
+                        value={newName}
+                        onChange={(event) => setNewName(event.target.value)}
+                    />
                 </div>
                 <div>
                     <button type="submit">add</button>
                 </div>
             </form>
+
             <h2>Numbers</h2>
-            ...
-        </div>
+
+            {persons.map((person) => (
+                <p key={person.name}>{person.name}</p>
+            ))}
+        </>
     );
 };
 
