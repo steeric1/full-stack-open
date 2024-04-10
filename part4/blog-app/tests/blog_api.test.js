d2f9554bc5580f009c4f58061970ee3c876cb2e4
Nikko Kujanpää <nikko.kujanpaa@gmail.com>
2024-04-10 16:52:30 +0300

    finish e4.9

--- part4/blog-app/tests/blog_api.test.js
+++ part4/blog-app/tests/blog_api.test.js
@@ -6,6 +6,7 @@ const supertest = require("supertest");
 const app = require("../app");
 const Blog = require("../models/blog");
 const { initialBlogs } = require("./test_helper");
+const _ = require("lodash");
 
 const api = supertest(app);
 
@@ -15,13 +16,24 @@ describe("blog api", () => {
         await Blog.insertMany(initialBlogs);
     });
 
-    it("should retrieve correct number of blogs", async () => {
+    it("should return correct number of blogs", async () => {
         const response = await api
             .get("/api/blogs")
             .expect(200)
             .expect("Content-Type", /application\/json/);
 
         assert.strictEqual(response.body.length, initialBlogs.length);
+    });
+
+    it("should return blogs with field 'id'", async () => {
+        const response = await api.get("/api/blogs");
+
+        const numBlogs = response.body.length;
+        const numWithId = response.body.filter((blog) =>
+            _.has(blog, "id")
+        ).length;
+
+        assert.strictEqual(numBlogs, numWithId);
     });
 
     after(async () => {
