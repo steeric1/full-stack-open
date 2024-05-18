df1489cd8965a1b9f8c3d3c17d542447f0117639
Nikko Kujanpää <nikko.kujanpaa@gmail.com>
2024-05-18 13:47:19 +0300

    finish e5.2

--- part5/blog-app-frontend/src/App.jsx
+++ part5/blog-app-frontend/src/App.jsx
@@ -14,15 +14,29 @@ const App = () => {
         blogService.getAll().then((blogs) => setBlogs(blogs));
     }, []);
 
+    useEffect(() => {
+        const user = localStorage.getItem("loggedInUser");
+        if (user) {
+            setUser(JSON.parse(user));
+        }
+    }, []);
+
     const handleLogin = async (event) => {
         event.preventDefault();
 
         try {
-            const { token, ...user } = await loginService.login(username, pass);
+            const user = await loginService.login(username, pass);
+
             setUser(user);
+            localStorage.setItem("loggedInUser", JSON.stringify(user));
         } catch (error) {
             console.log("login failed", error);
         }
+    };
+
+    const handleLogout = () => {
+        setUser(null);
+        localStorage.removeItem("loggedInUser");
     };
 
     const loginForm = () => (
@@ -56,11 +70,18 @@ const App = () => {
     const blogList = () => (
         <div>
             <h2>blogs</h2>
-            <p>{user.name} logged in</p>
+            {userInfo()}
             {blogs.map((blog) => (
                 <Blog key={blog.id} blog={blog} />
             ))}
         </div>
+    );
+
+    const userInfo = () => (
+        <p>
+            {user.name} logged in{" "}
+            <button onClick={handleLogout}>log out</button>
+        </p>
     );
 
     return <div>{user ? blogList() : loginForm()}</div>;
