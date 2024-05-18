3b67a68730b138a18ad60056806bd353ef9c22d3
Nikko Kujanpää <nikko.kujanpaa@gmail.com>
2024-05-18 20:47:32 +0300

    finish e5.10

--- part5/blog-app-frontend/src/App.jsx
+++ part5/blog-app-frontend/src/App.jsx
@@ -144,20 +144,22 @@ const App = () => {
 
             <br />
 
-            {blogs.map((blog) => (
-                <Blog
-                    key={blog.id}
-                    blog={blog}
-                    handleLike={async () => {
-                        const liked = await blogService.like(blog);
-                        setBlogs(
-                            blogs.map((blog) =>
-                                blog.id === liked.id ? liked : blog
-                            )
-                        );
-                    }}
-                />
-            ))}
+            {blogs
+                .sort((a, b) => b.likes - a.likes)
+                .map((blog) => (
+                    <Blog
+                        key={blog.id}
+                        blog={blog}
+                        handleLike={async () => {
+                            const liked = await blogService.like(blog);
+                            setBlogs(
+                                blogs.map((blog) =>
+                                    blog.id === liked.id ? liked : blog
+                                )
+                            );
+                        }}
+                    />
+                ))}
         </div>
     ) : (
         <LoginForm handleSubmit={handleLogin} />
