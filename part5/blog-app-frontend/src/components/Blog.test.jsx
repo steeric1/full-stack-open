b449d63e18660c700eb13bb2109f99f73058a8bd
Nikko Kujanpää <nikko.kujanpaa@gmail.com>
2024-05-19 12:25:24 +0300

    finish e5.14

--- part5/blog-app-frontend/src/components/Blog.test.jsx
+++ part5/blog-app-frontend/src/components/Blog.test.jsx
@@ -1,31 +1,50 @@
-import { expect } from "vitest";
+import { expect, test, describe, beforeEach } from "vitest";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import Blog from "./Blog";
 
-test("<Blog /> initially renders only title and author", () => {
+describe("<Blog />", () => {
     const blog = {
         title: "This is a blog post",
         author: "Linus Gates",
         likes: 3,
         url: "linus.gates.com",
+        user: {
+            name: "Steve Torvalds",
+        },
     };
 
-    render(
-        <Blog
-            blog={blog}
-            handleLike={() => {}}
-            showRemove={false}
-            handleRemove={() => {}}
-        />
-    );
+    beforeEach(() => {
+        render(
+            <Blog
+                blog={blog}
+                handleLike={() => {}}
+                showRemove={false}
+                handleRemove={() => {}}
+            />
+        );
+    });
 
-    screen.getByText(blog.title);
-    screen.getByText(blog.author);
+    test("initially renders only title and author", () => {
+        screen.getByText(blog.title);
+        screen.getByText(blog.author);
 
-    const likes = screen.queryByText(`likes ${blog.likes}`);
-    expect(likes).toBeNull();
+        const likes = screen.queryByText(`likes ${blog.likes}`);
+        expect(likes).toBeNull();
 
-    const url = screen.queryByText(blog.url);
-    expect(url).toBeNull();
+        const url = screen.queryByText(blog.url);
+        expect(url).toBeNull();
+    });
+
+    test("renders url, likes and user when expanded", async () => {
+        const user = userEvent.setup();
+        const button = screen.getByText("show");
+
+        await user.click(button);
+
+        screen.queryByText(`likes ${blog.likes}`);
+        screen.getByText(blog.url);
+        screen.getByText(blog.user.name);
+    });
 });
