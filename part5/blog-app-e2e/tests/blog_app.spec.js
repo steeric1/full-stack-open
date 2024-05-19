5944073b378cd42952bf1f962a8a42394f90a18a
Nikko Kujanpää <nikko.kujanpaa@gmail.com>
2024-05-19 15:06:27 +0300

    finish e5.18

--- part5/blog-app-e2e/tests/blog_app.spec.js
+++ part5/blog-app-e2e/tests/blog_app.spec.js
@@ -1,14 +1,17 @@
 const { test, expect, beforeEach, describe } = require("@playwright/test");
+const { loginWith } = require("./helper");
+
+const user = {
+    name: "Markku Kankainen",
+    username: "markku",
+    password: "markunsalis",
+};
 
 describe("Blog App", () => {
     beforeEach(async ({ page, request }) => {
         await request.post("http:localhost:3003/api/testing/reset");
         await request.post("http://localhost:3003/api/users", {
-            data: {
-                name: "Markku Kankainen",
-                username: "markku",
-                password: "markunsalis",
-            },
+            data: user,
         });
 
         await page.goto("http://localhost:5173");
@@ -21,5 +24,29 @@ describe("Blog App", () => {
 
         await expect(page.getByTestId("username")).toBeVisible();
         await expect(page.getByTestId("password")).toBeVisible();
+
+        await expect(page.getByRole("button", { name: "login" })).toBeVisible();
+    });
+
+    describe("Login", () => {
+        test("succeeds with correct credentials", async ({ page }) => {
+            await loginWith(page, user.username, user.password);
+
+            await expect(
+                page.getByText(`${user.name} logged in`)
+            ).toBeVisible();
+        });
+
+        test("fails with wrong credentials", async ({ page }) => {
+            await loginWith(page, user.username, "wrong");
+
+            await expect(
+                page.getByText(`${user.name} logged in`)
+            ).not.toBeVisible();
+
+            await expect(
+                page.getByText("wrong username or password")
+            ).toBeVisible();
+        });
     });
 });
