4931ea5f2270c236350384b3541b467abd4164b6
Nikko Kujanpää <nikko.kujanpaa@gmail.com>
2024-05-21 15:25:10 +0300

    finish e6.20

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,18 +1,32 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
 import AnecdoteForm from "./components/AnecdoteForm";
 import Notification from "./components/Notification";
+import { getAnecdotes } from "./requests";
 
 const App = () => {
     const handleVote = (anecdote) => {
         console.log("vote");
     };
 
-    const anecdotes = [
-        {
-            content: "If it hurts, do it more often",
-            id: "47145",
-            votes: 0,
-        },
-    ];
+    const result = useQuery({
+        queryKey: ["anecdotes"],
+        queryFn: getAnecdotes,
+        retry: 1,
+    });
+
+    if (result.isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (result.isError) {
+        return (
+            <div>anecdote service not available due to problems in server</div>
+        );
+    }
+
+    const anecdotes = result.data;
 
     return (
         <div>
