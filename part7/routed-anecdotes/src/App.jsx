85deb9eaceba46e7a55eb2c038e0cff7be99a8be
Nikko Kujanpää <nikko.kujanpaa@gmail.com>
2024-05-22 09:58:53 +0300

    finish e7.1

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 const Menu = () => {
     const padding = {
@@ -6,15 +7,15 @@ const Menu = () => {
     };
     return (
         <div>
-            <a href="#" style={padding}>
+            <Link to="/" style={padding}>
                 anecdotes
-            </a>
-            <a href="#" style={padding}>
+            </Link>
+            <Link to="/create" style={padding}>
                 create new
-            </a>
-            <a href="#" style={padding}>
+            </Link>
+            <Link to="/about" style={padding}>
                 about
-            </a>
+            </Link>
         </div>
     );
 };
@@ -152,14 +153,26 @@ const App = () => {
     };
 
     return (
-        <div>
-            <h1>Software anecdotes</h1>
-            <Menu />
-            <AnecdoteList anecdotes={anecdotes} />
-            <About />
-            <CreateNew addNew={addNew} />
-            <Footer />
-        </div>
+        <Router>
+            <div>
+                <h1>Software anecdotes</h1>
+                <Menu />
+
+                <Routes>
+                    <Route
+                        path="/"
+                        element={<AnecdoteList anecdotes={anecdotes} />}
+                    />
+                    <Route path="/about" element={<About />} />
+                    <Route
+                        path="/create"
+                        element={<CreateNew addNew={addNew} />}
+                    />
+                </Routes>
+
+                <Footer />
+            </div>
+        </Router>
     );
 };
 
