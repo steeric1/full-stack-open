e39db0f0709683740e9458223b3bbebfe89a60dd
Nikko Kujanpää <nikko.kujanpaa@gmail.com>
2024-05-24 17:57:24 +0300

    finish e8.2

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -98,16 +98,26 @@ let books = [
 */
 
 const typeDefs = `
-  type Query {
-    bookCount: Int!
-    authorCount: Int!
-  }
+    type Book {
+        title: String!
+        published: Int!
+        author: String!
+        id: ID!
+        genres: [String!]!
+    }
+
+    type Query {
+        bookCount: Int!
+        authorCount: Int!
+        allBooks: [Book!]!
+    }
 `;
 
 const resolvers = {
     Query: {
         bookCount: () => books.length,
         authorCount: () => authors.length,
+        allBooks: () => books,
     },
 };
 
